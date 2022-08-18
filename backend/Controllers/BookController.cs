119b64d9c28ff2278fe9a90fab3a0256696ac844
Pekka Alaluukas <pekka.alaluukas@gmail.com>
2022-08-18 11:15:54 +0300

    authorization works

--- backend/Controllers/BookController.cs
+++ backend/Controllers/BookController.cs
@@ -4,6 +4,7 @@ using Microsoft.AspNetCore.Mvc;
 namespace backend.Controllers
 {
     [Route("api/[controller]")]
+    [BasicAuthorization]
     public class BookController : ControllerBase
     {
         public BookController(Database db)
