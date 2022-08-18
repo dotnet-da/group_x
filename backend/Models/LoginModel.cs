98f0f9187327e215aa2da20d6e0c31a5b6831aba
Pekka Alaluukas <pekka.alaluukas@gmail.com>
2022-08-18 11:02:00 +0300

    login system works

---
+++ backend/Models/LoginModel.cs
@@ -0,0 +1,62 @@
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Common;
+using System.Threading.Tasks;
+using MySqlConnector;
+
+namespace backend
+{
+    public class Login
+    {
+        public int  id_user { get; set; }
+        public string username { get; set; }
+        public string password { get; set; }
+
+        internal Database Db { get; set; }
+
+        public Login()
+        {
+        }
+
+        internal Login(Database db)
+        {
+            Db = db;
+        }
+
+
+        public async Task<string> GetPassword(string username)
+        {
+            using var cmd = Db.Connection.CreateCommand();
+            cmd.CommandText = @"SELECT `password`  FROM `user` WHERE `username` = @username";
+            cmd.Parameters.Add(new MySqlParameter
+            {
+                ParameterName = "@username",
+                DbType = DbType.String,
+                Value =  username,
+            });
+            var result = await ReadPassword(await cmd.ExecuteReaderAsync());
+            //return lengt(result) > 0 ? result : null;
+            return result;
+        }
+
+        private async Task<string> ReadPassword(DbDataReader reader)
+        {
+            var loginUser = new Login();
+            using (reader)
+            {
+                while (await reader.ReadAsync())
+                {
+                    var user = new Login(Db)
+                    {
+                        password = reader.GetString(0)
+                    };
+                    loginUser=user;
+                }
+            }
+
+            return loginUser.password;
+        }
+        
+    
+    }
+}
